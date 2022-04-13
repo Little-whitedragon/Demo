750d854ec4e3e25e97a3cc1bc7a2748084784a86
Little-whitedragon <991608279@qq.com>
2022-04-13 15:10:32 +0800

    修改项目

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+<div></div>
+</template>
+
+<script>
+// @ is an alias to /src
+// eslint-disable-next-line no-unused-vars
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
